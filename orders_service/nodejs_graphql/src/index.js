eceed643f062ad0ca0191335dfb0ad8cb84fd53e
Randy <randyma01@gmail.com>
2019-04-27 15:43:40 -0600

    Creating Catalog Service (Node.js+GrahpQL)

--- orders_service/nodejs_graphql/src/index.js
+++ orders_service/nodejs_graphql/src/index.js
@@ -1,31 +1,27 @@
 import Hapi from 'hapi';
 import mongoose from 'mongoose';
-import statsRoutes from './api/v1/stats';
-import usersRoutes from './api/v1/users';
 
-const server = Hapi.server({
-  host: 'localhost',
-  port: 8000
-});
+const init = async () => {
+  const server = Hapi.server({
+    port: 8000,
+    host: 'localhost'
+  });
 
-statsRoutes(server);
-usersRoutes(server);
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello World!';
+    }
+  });
 
-const start = async function() {
-  try {
-    /* mongoose.connect(
-    );
-    mongoose.connection.once('open', () => {
-      console.log('connected to database');
-    }); */
-
-    await server.start();
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  console.log('Server running at:', server.info.uri);
+  await server.start();
+  console.log('Server running on %ss', server.info.uri);
 };
 
-start();
+process.on('unhandledRejection', err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
